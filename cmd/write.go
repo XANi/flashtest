1c6867c7591a6e040f6e602687c5547e425111d5
Mariusz Gronczewski <xani666@gmail.com>
2019-05-03 22:20:13 +0200

    separate routine for verify

--- cmd/write.go
+++ cmd/write.go
@@ -34,6 +34,15 @@ func WriteFile(filename string, filesize int) {
 		_ = err // handle write errors
 	}
 }
+type verifyQ struct {
+	block []byte
+	offset int
+}
+type reportQ struct {
+	pos int
+	size int
+	ok bool
+}
 func VerifyFile(filename string, filesize int) {
 	dev, err := blockdev.NewFromFile(filename)
 	if err != nil {
@@ -52,20 +61,44 @@ func VerifyFile(filename string, filesize int) {
 	if filesize > checkedSize {
 		log.Warningf("File is not aligned to data block size[%d]. Will not touch last %d bytes", datastore.DataBlockSize, filesize-checkedSize)
 	}
+	vq := make(chan verifyQ,5)
+	end := make(chan bool,1)
+	go func() {
+		  for qe := range vq {
+		  	ok,out,errlist,err:=verifyBlock(qe.block,qe.offset)
+			  if ok {
+				  log.Infof("decoded block %d: %s", qe.offset, string(out))
+			  }
+			  if len(errlist) > 0 {
+				  log.Warning("  found errors")
+				  for _, e := range errlist {
+					  log.Warningf("  [%d] error: %+v", qe.offset, e)
+				  }
+			  }
+			  if err != nil {
+				  log.Errorf("error decoding block %d: %s",qe.offset,err)
+			  }
+		  }
+		  end <- true
+	} ()
 	for i := 0; i < dataBlocks; i++ {
 		offset := i * datastore.DataBlockSize
 		data,_ := dev.Read(offset,datastore.DataBlockSize)
-		out,errlist,err := datastore.DecodeDataBlock(data)
-		log.Infof("decoded block %d: %s", i, string(out))
-		if len(errlist) > 0 {
-			log.Warning("  found errors")
-			for _, e := range errlist {
-				log.Warningf("  [%d] error: %+v", i, e)
-			}
-		}
-		if err != nil {
-			log.Errorf("error decoding block %d: %s",i,err)
-		}
+		vq <- verifyQ{block:data,offset:offset}
 	}
+	close(vq)
+	_ = <- end
+}
+
+func verifyBlock(block []byte,offset int) (ok bool, data []byte,errl []datastore.DecodeError, err error) {
+	out,errlist,err := datastore.DecodeDataBlock(block)
+	if len(errlist) < 1 && err == nil {
+		return true, out, errlist, nil
+	}
+	if err != nil {
+		return false, []byte{}, errlist, err
+	}
+	return true,out,errlist,nil
 
 }
+
